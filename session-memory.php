60a6014e8e8470b690ea071e15bdf971753e480e
tahyhax <dimon4i4ek@ukr.net>
2020-07-27 03:03:36 +0300

    refactoring  and  set  default timezome

--- session-memory.php
+++ session-memory.php
@@ -1,24 +1,23 @@
 <?php
 ini_set('session.gc_maxlifetime', 120960);
 ini_set('session.cookie_lifetime', 120960);
+
+date_default_timezone_set("Europe/Kiev");
+
 $_POST = json_decode(file_get_contents("php://input"), true);
 
 session_start();
 
-$sessionId = session_id();
-
 if (isset($_POST['form'])) {
-    header('Content-Type: application/json; charset=utf-8');
 
-    saveUserInfo($_POST['form'], $sessionId);
+    saveUserInfo($_POST['form']);
 
-    sendResponse($_SESSION[$sessionId]);
+    sendResponse($_SESSION);
 
 
 }
 
 if (isset($_POST['sessionClear'])) {
-    header('Content-Type: application/json; charset=utf-8');
     $clearSession = sessionClear();
     $data = [
         'message' => $clearSession
@@ -28,11 +27,11 @@ if (isset($_POST['sessionClear'])) {
     sendResponse($data);
 }
 
-if (!isset($_SESSION[$sessionId])) {
-    $_SESSION[$sessionId]['counter'] = 0;
+if (!isset($_SESSION['counter'])) {
+    $_SESSION['counter'] = 0;
 }
-$_SESSION[$sessionId]['counter']++;
-$_SESSION[$sessionId]['lastConnect'] = date('m/d/Y  H:i:s');
+$_SESSION['counter']++;
+$_SESSION['lastConnect'] = date('m/d/Y  H:i:s');
 
 
 /**
@@ -44,7 +43,7 @@ $_SESSION[$sessionId]['lastConnect'] = date('m/d/Y  H:i:s');
  */
 function sendResponse($data, $errorCode = null, $errorMessage = null)
 {
-
+    header('Content-Type: application/json; charset=utf-8');
     $response['data'] = $data;
     $response['error']['code'] = $errorCode;
     $response['error']['message'] = $errorMessage;
@@ -57,9 +56,9 @@ function sendResponse($data, $errorCode = null, $errorMessage = null)
  * save new  data  to session
  *
  * @param  array $params
- * @param string $sessionId current  session id
+ * // * @param string $sessionId current  session id
  */
-function saveUserInfo($params, $sessionId)
+function saveUserInfo($params)
 {
 
     $data = [];
@@ -67,11 +66,14 @@ function saveUserInfo($params, $sessionId)
     foreach ($params as $key => $value) {
         $data[$key] = trim(stripcslashes($value));
     }
-    $_SESSION[$sessionId]['info'] = $data;
 
     $yearOfBirth = getYearOfBirth($data['age']);
 
-    $_SESSION[$sessionId]['info']['yearOfBirth'] = $yearOfBirth;
+    $data['info']['yearOfBirth'] = $yearOfBirth;
+    $_SESSION['info'] = $data;
+
+
+//    add return $_SESSION
 
 }
 
@@ -102,7 +104,7 @@ function isInfo($data)
  * @param array $info
  * @return string
  */
-function renderUseInfo($info)
+function getUseInfoHtml($info)
 {
     $template = 'You are %s %s and you were born in %s.';
     return sprintf($template, $info["firstName"], $info["lastName"], $info["yearOfBirth"]);
@@ -121,233 +123,237 @@ function sessionClear()
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8"/>
-    <meta
-        name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-    />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <title>Document</title>
-    <style>
-        *,
-        *::before,
-        *::after {
-            -webkit-box-sizing: border-box;
-            box-sizing: border-box;
-        }
+  <meta charset="UTF-8"/>
+  <meta
+      name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+  />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+  <title>Document</title>
+  <style>
+    *,
+    *::before,
+    *::after {
+      -webkit-box-sizing: border-box;
+      box-sizing: border-box;
+    }
 
-        ul[class],
-        ol[class] {
-            padding: 0;
-        }
+    ul[class],
+    ol[class] {
+      padding: 0;
+    }
 
-        body,
-        h1,
-        h2,
-        h3,
-        h4,
-        p,
-        ul[class],
-        ol[class],
-        figure,
-        blockquote,
-        dl,
-        dd {
-            margin: 0;
-        }
+    body,
+    h1,
+    h2,
+    h3,
+    h4,
+    p,
+    ul[class],
+    ol[class],
+    figure,
+    blockquote,
+    dl,
+    dd {
+      margin: 0;
+    }
 
-        input,
-        select {
-            outline: none;
-        }
+    input,
+    select {
+      outline: none;
+    }
 
-        .wrap {
-            position: relative;
-            height: 100vh;
-            display: -webkit-box;
-            display: -ms-flexbox;
-            display: flex;
-            -webkit-box-pack: center;
-            -ms-flex-pack: center;
-            justify-content: center;
-            -webkit-box-align: center;
-            -ms-flex-align: center;
-            align-items: center;
-        }
+    .wrap {
+      position: relative;
+      height: 100vh;
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+      -webkit-box-pack: center;
+      -ms-flex-pack: center;
+      justify-content: center;
+      -webkit-box-align: center;
+      -ms-flex-align: center;
+      align-items: center;
+    }
 
-        .wrap__inner {
-            -webkit-box-flex: 50%;
-            -ms-flex: 50%;
-            flex: 0 0 50%;
-            max-width: 50%;
-        }
+    .wrap__inner {
+      -webkit-box-flex: 50%;
+      -ms-flex: 50%;
+      flex: 0 0 50%;
+      max-width: 50%;
+    }
 
-        .session__info {
-            text-align: center;
-            margin-bottom: 15px;
-        }
+    .session__info {
+      text-align: center;
+      margin-bottom: 15px;
+    }
 
-        .session__form {
-            margin-bottom: 15px;
-            max-width: 66.6666%;
-            margin: 0 auto;
-        }
+    .session__form {
+      margin-bottom: 15px;
+      max-width: 66.6666%;
+      margin: 0 auto;
+    }
 
-        .session-form {
-            display: -webkit-box;
-            display: -ms-flexbox;
-            display: flex;
-            -webkit-box-orient: vertical;
-            -webkit-box-direction: normal;
-            -ms-flex-direction: column;
-            flex-direction: column;
-            padding: 15px;
-            font-size: 1rem;
-        }
+    .session-form {
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+      -webkit-box-orient: vertical;
+      -webkit-box-direction: normal;
+      -ms-flex-direction: column;
+      flex-direction: column;
+      padding: 15px;
+      font-size: 1rem;
+    }
 
-        .session-form__item {
-            display: -webkit-box;
-            display: -ms-flexbox;
-            display: flex;
-            margin-bottom: 10px;
-        }
+    .session-form__item {
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+      margin-bottom: 10px;
+    }
 
-        .session-form__item label {
-            font-size: 0.9em;
-            -webkit-box-flex: 0;
-            -ms-flex: 0 0 25%;
-            flex: 0 0 25%;
-        }
+    .session-form__item label {
+      font-size: 0.9em;
+      -webkit-box-flex: 0;
+      -ms-flex: 0 0 25%;
+      flex: 0 0 25%;
+    }
 
-        .session-form__item input {
-            -webkit-box-flex: 1;
-            -ms-flex: 1;
-            flex: 1;
-        }
+    .session-form__item input {
+      -webkit-box-flex: 1;
+      -ms-flex: 1;
+      flex: 1;
+    }
 
-        .session-form__button {
-            margin-left: auto;
-        }
+    .session-form__button {
+      margin-left: auto;
+    }
 
-        .form-input,
-        .form-select {
-            padding: 5px;
-        }
+    .form-input,
+    .form-select {
+      padding: 5px;
+    }
 
-        .form-label {
-            display: -webkit-box;
-            display: -ms-flexbox;
-            display: flex;
-            font-weight: bold;
-        }
+    .form-label {
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+      font-weight: bold;
+    }
 
-        .form-label--center {
-            -webkit-box-align: center;
-            -ms-flex-align: center;
-            align-items: center;
-        }
+    .form-label--center {
+      -webkit-box-align: center;
+      -ms-flex-align: center;
+      align-items: center;
+    }
 
-        .form-button {
-            padding: 5px 10px;
-            font-size: 0.9em;
-            font-weight: bold;
-            cursor: pointer;
-        }
+    .form-button {
+      padding: 5px 10px;
+      font-size: 0.9em;
+      font-weight: bold;
+      cursor: pointer;
+    }
 
-        .connection-count {
-            font-size: 1.2rem;
-            font-weight: bold;
-            color: red;
-        }
+    .connection-count {
+      font-size: 1.2rem;
+      font-weight: bold;
+      color: red;
+    }
 
-        .connection-last {
-            font-style: italic;
-            color: green;
-        }
+    .connection-last {
+      font-style: italic;
+      color: green;
+    }
 
-        .session-clear {
-            position: absolute;
-            cursor: pointer;
-            bottom: 10%;
-            right: 10%;
-            padding: 15px;
-            border: none;
-            font-size: 1.1rem;
-            font-weight: bold;
-            background-color: #ffc107;
-        }
-    </style>
+    .session-clear {
+      position: absolute;
+      cursor: pointer;
+      bottom: 10%;
+      right: 10%;
+      padding: 15px;
+      border: none;
+      font-size: 1.1rem;
+      font-weight: bold;
+      background-color: #ffc107;
+    }
+  </style>
 </head>
 <body>
 <div class="wrap">
-    <div class="wrap__inner session">
-        <div class="session__info session-info">
-            <div class="session-info__text">
+  <div class="wrap__inner session">
+    <div class="session__info session-info">
+      <div class="session-info__text">
+          <?php
+          if (isInfo($_SESSION)) {
+              echo getUseInfoHtml($_SESSION['info']);
+          }
+          ?>
+        You have been here
+        <span class="session-info__connection-count connection-count"
+        >
+                    <?= $_SESSION['counter'] ?>
+                </span>
+          <?= ngettext("time", "times", $_SESSION['counter']) ?>
+        . Your the last visit was
+        <time
+            datetime="<?= $_SESSION['lastConnect'] ?>"
+            class="session-info__connection-last connection-last"
+        ><?= $_SESSION['lastConnect'] ?>
+        </time
+        >
+        .
+      </div>
+    </div>
+      <?php
+      if (!isInfo($_SESSION)) { ?>
+
+        <div class="session__form session-form">
+          <div class="session-form__item">
+            <label for="first-name" class="form-label form-label--center"
+            >First name</label
+            >
+            <input
+                required="true"
+                type="text"
+                name="firstName"
+                class="form-input"
+                id="first-name"
+            />
+          </div>
+          <div class="session-form__item">
+            <label for="last-name" class="form-label form-label--center"
+            >Last name</label
+            >
+            <input
+                required="true"
+                type="text"
+                name="lastName"
+                class="form-input"
+                id="last-name"
+            />
+          </div>
+          <div class="session-form__item">
+            <label for="age" class="form-label form-label--center">Age</label>
+            <select name="age" class="form-select" id="age">
                 <?php
-                if (isInfo($_SESSION[$sessionId])) {
-                    echo renderUseInfo($_SESSION[$sessionId]['info']);
+                for ($i = 1; $i <= 35; $i++) {
+                    $html = $i == 15 ? "<option selected value='$i'>$i</option>" : "<option  value='$i'>$i</option>";
+                    echo($html);
                 }
                 ?>
-                You have been here
-                <span class="session-info__connection-count connection-count"
-                ><?=$_SESSION[$sessionId]['counter']?></span> <?=ngettext("time", "times", $_SESSION[$sessionId]['counter'])?>. Your the last visit was
-                <time
-                    datetime="<?=$_SESSION[$sessionId]['lastConnect'] ?>"
-                    class="session-info__connection-last connection-last"
-                ><?=$_SESSION[$sessionId]['lastConnect'] ?>
-                </time
-                >
-                .
-            </div>
+            </select>
+          </div>
+
+          <button type="button" class="session-form__button form-button">
+            Send
+          </button>
         </div>
-        <?php
-        if (!isInfo($_SESSION[$sessionId])) { ?>
 
-            <div class="session__form session-form">
-                <div class="session-form__item">
-                    <label for="first-name" class="form-label form-label--center"
-                    >First name</label
-                    >
-                    <input
-                        required="true"
-                        type="text"
-                        name="firstName"
-                        class="form-input"
-                        id="first-name"
-                    />
-                </div>
-                <div class="session-form__item">
-                    <label for="last-name" class="form-label form-label--center"
-                    >Last name</label
-                    >
-                    <input
-                        required="true"
-                        type="text"
-                        name="lastName"
-                        class="form-input"
-                        id="last-name"
-                    />
-                </div>
-                <div class="session-form__item">
-                    <label for="age" class="form-label form-label--center">Age</label>
-                    <select name="age" class="form-select" id="age">
-                        <?php
-                        for ($i = 1; $i <= 35; $i++) {
-                            $html = $i == 15 ? "<option selected value='$i'>$i</option>" : "<option  value='$i'>$i</option>";
-                            echo($html);
-                        }
-                        ?>
-                    </select>
-                </div>
-
-                <button type="button" class="session-form__button form-button">
-                    Send
-                </button>
-            </div>
-
-        <?php } ?>
-    </div>
-    <button class="session-clear">Clear Session</button>
+      <?php } ?>
+  </div>
+  <button class="session-clear">Clear Session</button>
 </div>
 <script>
   const form = document.querySelector(".session-form");
---
+++ session-memory/README.md
@@ -0,0 +1,2 @@
+# session-memory
+- to start php -S localhost:8000
